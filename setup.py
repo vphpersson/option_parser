191de8a65110330b0126bd99751d963b91c3904b
vph <vph.persson@gmail.com>
2023-04-25 21:27:23 +0200

    Initial.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='option_parser',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'typed_argument_parser @ git+https://github.com/vphpersson/typed_argument_parser.git#egg=typed_argument_parser',
+        'toml'
+    ]
+)
